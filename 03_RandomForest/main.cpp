4e27bf794c5e9e8eac843dc9f754edfcad704261
Shinya Kitaoka <skitaoka@gmail.com>
2014-07-27 03:25:49 +0900

    cosmetic change

--- 03_RandomForest/main.cpp
+++ 03_RandomForest/main.cpp
@@ -11,354 +11,354 @@
 #include <map>
 #include <vector>
 
-// Œˆ’è–Ø‚Ìƒf[ƒ^\‘¢
+// æ±ºå®šæœ¨ã®ãƒ‡ãƒ¼ã‚¿æ§‹é€ 
 namespace
 {
-	template <typename Data>
-	struct decision_tree
-	{
-		virtual int operator () (Data const & data) const = 0;
-	};
+  template <typename Data>
+  struct decision_tree
+  {
+    virtual int operator () (Data const & data) const = 0;
+  };
 
-	template <typename Data>
-	struct dt_node: public decision_tree<Data>
-	{
-		dt_node(int const question, std::vector<std::shared_ptr<decision_tree<Data>>> children)
-			: question_(question)
-			, children_(children)
-		{
-		}
+  template <typename Data>
+  struct dt_node : public decision_tree<Data>
+  {
+    dt_node(int const question, std::vector<std::shared_ptr<decision_tree<Data>>> children)
+      : question_(question)
+      , children_(children)
+    {
+    }
 
-		virtual int operator () (Data const & data) const
-		{
-			return (*children_[data(question_)])(data);
-		}
+    virtual int operator () (Data const & data) const
+    {
+      return (*children_[data(question_)])(data);
+    }
 
-	private:
-		int const question_; // ¿–â”Ô†
+  private:
+    int const question_; // è³ªå•ç•ªå·
 
-		std::vector<std::shared_ptr<decision_tree<Data>>> children_;
-	};
+    std::vector<std::shared_ptr<decision_tree<Data>>> children_;
+  };
 
-	template <typename Data>
-	struct dt_leaf: public decision_tree<Data>
-	{
-		dt_leaf(int const klass)
-			: klass_(klass)
-		{
-		}
+  template <typename Data>
+  struct dt_leaf : public decision_tree<Data>
+  {
+    dt_leaf(int const klass)
+      : klass_(klass)
+    {
+    }
 
-		virtual int operator () (Data const & data) const
-		{
-			return klass_;
-		}
+    virtual int operator () (Data const & data) const
+    {
+      return klass_;
+    }
 
-	private:
-		int const klass_; // “š‚¦‚ÌƒNƒ‰ƒX
-	};
+  private:
+    int const klass_; // ç­”ãˆã®ã‚¯ãƒ©ã‚¹
+  };
 
-	template <typename Data>
+  template <typename Data>
   double entropy_function(int const question, std::size_t const i, std::vector<Data> const & data)
-	{
-		// ¿–â question ‚É‚Â‚¢‚ÄA“š‚¦‚ª i ‚É‚È‚é—v‘f‚É‚Â‚¢‚Ä‚ÌƒNƒ‰ƒX‚Ì•ª•z‚©‚çƒGƒ“ƒgƒƒs[‚ğ‹‚ß‚é
-		std::vector<double> p(iris_data::num_classes, 0);
-		for (Data const & d : data) {
+  {
+    // è³ªå• question ã«ã¤ã„ã¦ã€ç­”ãˆãŒ i ã«ãªã‚‹è¦ç´ ã«ã¤ã„ã¦ã®ã‚¯ãƒ©ã‚¹ã®åˆ†å¸ƒã‹ã‚‰ã‚¨ãƒ³ãƒˆãƒ­ãƒ”ãƒ¼ã‚’æ±‚ã‚ã‚‹
+    std::vector<double> p(iris_data::num_classes, 0);
+    for (Data const & d : data) {
       if (d(question) == i) {
-				++p[d.klass()];
-			}
-		}
-		double const count = std::accumulate(std::begin(p), std::end(p), 0.0);
-		if (count <= 0) {
-			return 0.0; // —v‘f‚ª‹ó‚Ì‚Æ‚«AƒGƒ“ƒgƒƒs[ŠÖ”‚Ì’l‚Í 0
-		}
-		std::transform(std::begin(p), std::end(p), std::begin(p),
-			std::bind1st(std::multiplies<double>(), 1.0 / count));
+        ++p[d.klass()];
+      }
+    }
+    double const count = std::accumulate(std::begin(p), std::end(p), 0.0);
+    if (count <= 0) {
+      return 0.0; // è¦ç´ ãŒç©ºã®ã¨ãã€ã‚¨ãƒ³ãƒˆãƒ­ãƒ”ãƒ¼é–¢æ•°ã®å€¤ã¯ 0
+    }
+    std::transform(std::begin(p), std::end(p), std::begin(p),
+      std::bind1st(std::multiplies<double>(), 1.0 / count));
 
-		double retval
+    double retval
 #if 1
-		// ƒGƒ“ƒgƒƒs[
-		= 0.0;
-		for (std::size_t i = 0; i < iris_data::num_classes; ++i) {
-			if (p[i] > 0.0) {
-				retval -= p[i] * std::log(p[i]);
-			}
-		}
+      // ã‚¨ãƒ³ãƒˆãƒ­ãƒ”ãƒ¼
+      = 0.0;
+    for (std::size_t i = 0; i < iris_data::num_classes; ++i) {
+      if (p[i] > 0.0) {
+        retval -= p[i] * std::log(p[i]);
+      }
+    }
 #else
-		// ƒWƒjŒW”
-		= 1.0;
-		for (std::size_t i = 0; i < iris_data::num_classes; ++i) {
-			retval -= p[i] * p[i];
-		}
+      // ã‚¸ãƒ‹ä¿‚æ•°
+      = 1.0;
+    for (std::size_t i = 0; i < iris_data::num_classes; ++i) {
+      retval -= p[i] * p[i];
+    }
 #endif
-		return retval;
-	}
+    return retval;
+  }
 
-	// î•ñƒQƒCƒ“‚ğŒvZ‚·‚é
-	template <typename Data>
+  // æƒ…å ±ã‚²ã‚¤ãƒ³ã‚’è¨ˆç®—ã™ã‚‹
+  template <typename Data>
   double find_information_gain(int const question, std::vector<Data> const & data)
-	{
+  {
     std::vector<int> frequency(iris_data::num_dimensions, 0);
     for (Data const & datum : data) {
       ++frequency[datum(question)];
-		}
+    }
 
-		// ƒm[ƒh“à‚Å‘æˆê€‚Í•Ï‰»‚µ‚È‚¢‚Ì‚ÅŒvZ‚µ‚È‚¢
-		double retval = 0.0;
-		for (std::size_t i = 0; i < iris_data::num_dimensions; ++i) {
+    // ãƒãƒ¼ãƒ‰å†…ã§ç¬¬ä¸€é …ã¯å¤‰åŒ–ã—ãªã„ã®ã§è¨ˆç®—ã—ãªã„
+    double retval = 0.0;
+    for (std::size_t i = 0; i < iris_data::num_dimensions; ++i) {
       retval -= frequency[i] * entropy_function(question, i, data);
-		}
+    }
     return retval / data.size();
-	}
+  }
 
-	// Œˆ’è–Ø‚ğ\’z‚·‚é
-	template <typename Data>
-	std::shared_ptr<decision_tree<Data>>
-		build_decision_tree(
-			std::vector<int > const & questions,
-			std::vector<Data> const & data)
-	{
-		// ƒf[ƒ^‚ª–³‚¢‚Æ‚«‚ÍƒfƒtƒHƒ‹ƒg’l: 0
-		if (data.empty()) {
-			return std::shared_ptr<decision_tree<Data>>(new dt_leaf<Data>(0));
-		}
+  // æ±ºå®šæœ¨ã‚’æ§‹ç¯‰ã™ã‚‹
+  template <typename Data>
+  std::shared_ptr<decision_tree<Data>>
+    build_decision_tree(
+    std::vector<int > const & questions,
+    std::vector<Data> const & data)
+  {
+    // ãƒ‡ãƒ¼ã‚¿ãŒç„¡ã„ã¨ãã¯ãƒ‡ãƒ•ã‚©ãƒ«ãƒˆå€¤: 0
+    if (data.empty()) {
+      return std::shared_ptr<decision_tree<Data>>(new dt_leaf<Data>(0));
+    }
 
-		// ¿–â‚ª‹ó‚Ì‚Æ‚«‚Í‘½”Œˆ‚Å
+    // è³ªå•ãŒç©ºã®ã¨ãã¯å¤šæ•°æ±ºã§
     if (questions.empty()) {
-			std::vector<std::size_t> bin(iris_data::num_classes, 0);
+      std::vector<std::size_t> bin(iris_data::num_classes, 0);
       for (Data const & datum : data) {
         ++bin[datum.klass()];
-			}
-			auto it = std::max_element(std::begin(bin), std::end(bin));
-			int const klass = static_cast<int>(std::distance(std::begin(bin), it));
-			return std::shared_ptr<decision_tree<Data>>(new dt_leaf<Data>(klass));
-		}    
+      }
+      auto it = std::max_element(std::begin(bin), std::end(bin));
+      int const klass = static_cast<int>(std::distance(std::begin(bin), it));
+      return std::shared_ptr<decision_tree<Data>>(new dt_leaf<Data>(klass));
+    }
 
-		// ‚·‚Å‚É‘S‚Ä“¯‚¶ƒNƒ‰ƒX‚È‚ç‚»‚ÌƒNƒ‰ƒX‚Å
-		if (std::all_of(std::begin(data), std::end(data), [&data](Data const & d){ return data.front().klass() == d.klass(); })) {
-			return std::shared_ptr<decision_tree<Data>>(new dt_leaf<Data>(data.front().klass()));
-		}
+    // ã™ã§ã«å…¨ã¦åŒã˜ã‚¯ãƒ©ã‚¹ãªã‚‰ãã®ã‚¯ãƒ©ã‚¹ã§
+    if (std::all_of(std::begin(data), std::end(data), [&data](Data const & d){ return data.front().klass() == d.klass(); })) {
+      return std::shared_ptr<decision_tree<Data>>(new dt_leaf<Data>(data.front().klass()));
+    }
 
-		// ‘S‚Ä‚Ì¿–â‚ğ‚·
-		double information_gain_max = -std::numeric_limits<double>::infinity();
+    // å…¨ã¦ã®è³ªå•ã‚’è©¦ã™
+    double information_gain_max = -std::numeric_limits<double>::infinity();
     int good_question = 0;
     for (int question : questions) {
       double const information_gain = find_information_gain(question, data);
-			if (information_gain_max < information_gain) {
-				information_gain_max = information_gain;
+      if (information_gain_max < information_gain) {
+        information_gain_max = information_gain;
         good_question = question;
-			}
-		}
+      }
+    }
 
-		// ˆê”Ô‚æ‚©‚Á‚½¿–â‚ÅAÄ‹A“I‚É‚Â‚­‚é
+    // ä¸€ç•ªã‚ˆã‹ã£ãŸè³ªå•ã§ã€å†å¸°çš„ã«ã¤ãã‚‹
     std::vector<std::shared_ptr<decision_tree<Data>>>
-      children(Data::num_dimensions); // qƒm[ƒh‚ÌW‡
+      children(Data::num_dimensions); // å­ãƒãƒ¼ãƒ‰ã®é›†åˆ
 
-    // Ì—p‚µ‚½¿–â‚ğœ‚¢‚½¿–â‚ÌW‡‚ğ‚Â‚­‚é
+    // æ¡ç”¨ã—ãŸè³ªå•ã‚’é™¤ã„ãŸè³ªå•ã®é›†åˆã‚’ã¤ãã‚‹
     std::vector<int> new_questions;
     for (int const question : questions) {
       if (question != good_question) {
         new_questions.push_back(question);
-			}
-		}
+      }
+    }
 
-		for (std::size_t i = 0; i < Data::num_dimensions; ++i) {
-			std::vector<Data> new_data;
-			for (Data const & d : data) {
+    for (std::size_t i = 0; i < Data::num_dimensions; ++i) {
+      std::vector<Data> new_data;
+      for (Data const & d : data) {
         if (i == d(good_question)) {
-					new_data.push_back(d); // good_question ‚É i ‚Æ‚¾‚¯“š‚¦‚éƒf[ƒ^‚ÌW‡‚ğV‹Kƒf[ƒ^‚Æ‚µ‚Ä‚Â‚­‚é
-				}
-			}
+          new_data.push_back(d); // good_question ã« i ã¨ã ã‘ç­”ãˆã‚‹ãƒ‡ãƒ¼ã‚¿ã®é›†åˆã‚’æ–°è¦ãƒ‡ãƒ¼ã‚¿ã¨ã—ã¦ã¤ãã‚‹
+        }
+      }
 
-			children[i] = build_decision_tree(new_questions, new_data);
-		}
-		return std::shared_ptr<decision_tree<Data>>(new dt_node<Data>(good_question, std::move(children)));
-	}
+      children[i] = build_decision_tree(new_questions, new_data);
+    }
+    return std::shared_ptr<decision_tree<Data>>(new dt_node<Data>(good_question, std::move(children)));
+  }
 }
 
-// ƒ†[ƒeƒBƒŠƒeƒB
+// ãƒ¦ãƒ¼ãƒ†ã‚£ãƒªãƒ†ã‚£
 namespace
 {
-	// ˜A‘±’l‚ğ—£U•Ï”‚É•ÏŠ·‚·‚é
-	template <std::size_t N>
-	std::vector<int> d2i(std::vector<double> data)
-	{
-		std::vector<double> sorted_data(data);
-		std::sort(std::begin(sorted_data), std::end(sorted_data));
+  // é€£ç¶šå€¤ã‚’é›¢æ•£å¤‰æ•°ã«å¤‰æ›ã™ã‚‹
+  template <std::size_t N>
+  std::vector<int> d2i(std::vector<double> data)
+  {
+    std::vector<double> sorted_data(data);
+    std::sort(std::begin(sorted_data), std::end(sorted_data));
 
-		// “K“–‚É‹æØ‚é
-		std::vector<double> threshold(N);
-		for (std::size_t i = 1; i < N; ++i) {
-			threshold[i-1] = sorted_data[sorted_data.size() * i / N];
-		}
-		threshold[N-1] = sorted_data.back();
+    // é©å½“ã«åŒºåˆ‡ã‚‹
+    std::vector<double> threshold(N);
+    for (std::size_t i = 1; i < N; ++i) {
+      threshold[i - 1] = sorted_data[sorted_data.size() * i / N];
+    }
+    threshold[N - 1] = sorted_data.back();
 
-		// •ÏŠ·
-		std::vector<int> retval(data.size());
-		std::transform(std::begin(data), std::end(data),
-			std::begin(retval), [&](double const x) -> int
-		{
-			 int dim = 0;
-			 while (x > threshold[dim]) {
-				++dim;
-			 }
-			 return dim;
-		});
-		return retval;
-	}
+    // å¤‰æ›
+    std::vector<int> retval(data.size());
+    std::transform(std::begin(data), std::end(data),
+      std::begin(retval), [&](double const x) -> int
+    {
+      int dim = 0;
+      while (x > threshold[dim]) {
+        ++dim;
+      }
+      return dim;
+    });
+    return retval;
+  }
 }
 
-// ƒf[ƒ^
+// ãƒ‡ãƒ¼ã‚¿
 namespace
 {
-	struct iris_data
-	{
-		static std::size_t const num_classes    = 3; // •ª—Ş‚·‚éƒNƒ‰ƒX‚Ì”
-		static std::size_t const num_questions  = 4; // “Á’¥•Ï”‚Ì”
-		static std::size_t const num_dimensions = 8; // ˜A‘±•Ï”‚Ì—£U‰»‰ğ‘œ“x
+  struct iris_data
+  {
+    static std::size_t const num_classes = 3; // åˆ†é¡ã™ã‚‹ã‚¯ãƒ©ã‚¹ã®æ•°
+    static std::size_t const num_questions = 4; // ç‰¹å¾´å¤‰æ•°ã®æ•°
+    static std::size_t const num_dimensions = 8; // é€£ç¶šå¤‰æ•°ã®é›¢æ•£åŒ–è§£åƒåº¦
 
     iris_data(std::vector<int> answers, int const klass)
       : answers_(std::move(answers))
       , klass_(klass)
-		{
-		}
+    {
+    }
 
-		iris_data(iris_data const & rhs)
+    iris_data(iris_data const & rhs)
       : answers_(rhs.answers_)
       , klass_(rhs.klass_)
-		{
-		}
+    {
+    }
 
-		// question ”Ô–Ú‚Ì¿–â‚Ì“š‚¦‚ğ•Ô‚·
-		inline int operator () (int const question) const 
-		{
+    // question ç•ªç›®ã®è³ªå•ã®ç­”ãˆã‚’è¿”ã™
+    inline int operator () (int const question) const
+    {
       return answers_[question];
-		}
+    }
 
-		inline int klass() const
-		{
+    inline int klass() const
+    {
       return klass_;
-		}
+    }
 
     std::vector<int> const answers_;
     int              const klass_;
-	};
+  };
 }
 
 int main(int argc, char* argv[])
 {
-	std::vector<iris_data> data;
-	{
-		std::vector<double> q1d;
-		std::vector<double> q2d;
-		std::vector<double> q3d;
-		std::vector<double> q4d;
-		std::vector<int   > ans;
+  std::vector<iris_data> data;
+  {
+    std::vector<double> q1d;
+    std::vector<double> q2d;
+    std::vector<double> q3d;
+    std::vector<double> q4d;
+    std::vector<int   > ans;
 
-		// ƒf[ƒ^“Ç‚İ‚İ
-		{
-			std::map<std::string, int> t2i;
-			t2i["setosa"] = 0;
-			t2i["versicolor"] = 1;
-			t2i["virginica"] = 2;
+    // ãƒ‡ãƒ¼ã‚¿èª­ã¿è¾¼ã¿
+    {
+      std::map<std::string, int> t2i;
+      t2i["setosa"] = 0;
+      t2i["versicolor"] = 1;
+      t2i["virginica"] = 2;
 
-			std::ifstream in("iris.dat");
-			if (!in) {
-				std::clog << "Could not open file: " << argv[1] << '\n';
-				return 1;
-			}
-			while (in) {
-				int id;
-				double q1, q2, q3, q4;
-				std::string tag;
+      std::ifstream in("iris.dat");
+      if (!in) {
+        std::clog << "Could not open file: " << argv[1] << '\n';
+        return 1;
+      }
+      while (in) {
+        int id;
+        double q1, q2, q3, q4;
+        std::string tag;
 
-				in >> id >> q1 >> q2 >> q3 >> q4 >> tag;
-				if (tag.empty()) {
-					break;
-				}
+        in >> id >> q1 >> q2 >> q3 >> q4 >> tag;
+        if (tag.empty()) {
+          break;
+        }
 
-				q1d.push_back(q1);
-				q2d.push_back(q2);
-				q3d.push_back(q3);
-				q4d.push_back(q4);
-				ans.push_back(t2i.find(tag)->second);
-			}
-		}
+        q1d.push_back(q1);
+        q2d.push_back(q2);
+        q3d.push_back(q3);
+        q4d.push_back(q4);
+        ans.push_back(t2i.find(tag)->second);
+      }
+    }
 
-		// —£U•Ï”‚É•ÏŠ·
-		std::vector<int> q1i = d2i<iris_data::num_dimensions>(q1d);
-		std::vector<int> q2i = d2i<iris_data::num_dimensions>(q2d);
-		std::vector<int> q3i = d2i<iris_data::num_dimensions>(q3d);
-		std::vector<int> q4i = d2i<iris_data::num_dimensions>(q4d);
+    // é›¢æ•£å¤‰æ•°ã«å¤‰æ›
+    std::vector<int> q1i = d2i<iris_data::num_dimensions>(q1d);
+    std::vector<int> q2i = d2i<iris_data::num_dimensions>(q2d);
+    std::vector<int> q3i = d2i<iris_data::num_dimensions>(q3d);
+    std::vector<int> q4i = d2i<iris_data::num_dimensions>(q4d);
 
-		for (std::size_t i = 0, size = ans.size(); i < size; ++i) {
-			std::vector<int> q(4);
-			q[0] = q1i[i];
-			q[1] = q2i[i];
-			q[2] = q3i[i];
-			q[3] = q4i[i];
-			data.push_back(iris_data(std::move(q), ans[i]));
-		}
-	}
+    for (std::size_t i = 0, size = ans.size(); i < size; ++i) {
+      std::vector<int> q(4);
+      q[0] = q1i[i];
+      q[1] = q2i[i];
+      q[2] = q3i[i];
+      q[3] = q4i[i];
+      data.push_back(iris_data(std::move(q), ans[i]));
+    }
+  }
 
-	// Œˆ’è–Ø‚ÌŒÂ”
-	std::size_t const num_decision_trees = 100;
+  // æ±ºå®šæœ¨ã®å€‹æ•°
+  std::size_t const num_decision_trees = 100;
 
-	// ŠwK‚É—˜—p‚·‚é¿–â‚Ì”
-  // •ª—Ş‚È‚ç sqrt(num_questions), ‰ñ‹A‚È‚ç num_questions/3) ‚®‚ç‚¢‚ª‚¢‚¢‚ç‚µ‚¢
+  // å­¦ç¿’ã«åˆ©ç”¨ã™ã‚‹è³ªå•ã®æ•°
+  // åˆ†é¡ãªã‚‰ sqrt(num_questions), å›å¸°ãªã‚‰ num_questions/3) ãã‚‰ã„ãŒã„ã„ã‚‰ã—ã„
   // cf. http://ja.wikipedia.org/wiki/Random_forest
-	std::size_t const num_learn_questions = iris_data::num_questions / 2;
+  std::size_t const num_learn_questions = iris_data::num_questions / 2;
 
-	// ŠwK‚É—˜—p‚·‚éƒf[ƒ^‚Ì”
-	std::size_t const num_sample_size = data.size() * 2 / 3;
+  // å­¦ç¿’ã«åˆ©ç”¨ã™ã‚‹ãƒ‡ãƒ¼ã‚¿ã®æ•°
+  std::size_t const num_sample_size = data.size() * 2 / 3;
 
-	std::vector<std::size_t> questions(iris_data::num_questions);
-	std::iota(std::begin(questions), std::end(questions), 0);
+  std::vector<std::size_t> questions(iris_data::num_questions);
+  std::iota(std::begin(questions), std::end(questions), 0);
 
-	std::vector<std::size_t> indices(data.size());
-	std::iota(std::begin(indices), std::end(indices), 0);
+  std::vector<std::size_t> indices(data.size());
+  std::iota(std::begin(indices), std::end(indices), 0);
 
-	// Œˆ’è–Ø (‚ÌW‡)
-	std::vector<std::shared_ptr<decision_tree<iris_data>>> trees;
-	std::mt19937_64 engine(std::random_device("")());
-	for (std::size_t k = 0; k < num_decision_trees; ++k) {
-		// ¿–â‚ÌW‡‚ğƒ‰ƒ“ƒ_ƒ€‚É‚Â‚­‚é
-		std::shuffle(std::begin(questions), std::end(questions), engine);
-		std::vector<int> questions(std::begin(questions), std::begin(questions) + num_learn_questions);
+  // æ±ºå®šæœ¨ (ã®é›†åˆ)
+  std::vector<std::shared_ptr<decision_tree<iris_data>>> trees;
+  std::mt19937_64 engine(std::random_device("")());
+  for (std::size_t k = 0; k < num_decision_trees; ++k) {
+    // è³ªå•ã®é›†åˆã‚’ãƒ©ãƒ³ãƒ€ãƒ ã«ã¤ãã‚‹
+    std::shuffle(std::begin(questions), std::end(questions), engine);
+    std::vector<int> questions(std::begin(questions), std::begin(questions) + num_learn_questions);
 
-		// ƒf[ƒ^‚ÌW‡‚ğƒ‰ƒ“ƒ_ƒ€‚É‚Â‚­‚é
-		std::shuffle(std::begin(indices), std::end(indices), engine);
-		std::vector<iris_data> learn_data;
-		learn_data.reserve(num_sample_size);
+    // ãƒ‡ãƒ¼ã‚¿ã®é›†åˆã‚’ãƒ©ãƒ³ãƒ€ãƒ ã«ã¤ãã‚‹
+    std::shuffle(std::begin(indices), std::end(indices), engine);
+    std::vector<iris_data> learn_data;
+    learn_data.reserve(num_sample_size);
     std::transform(std::begin(indices), std::begin(indices) + num_sample_size,
       std::back_inserter(learn_data), [&data](int const i) { return data[i]; });
 
     trees.push_back(build_decision_tree(questions, learn_data));
-	}
+  }
 
-	// ‘S‚Ä”»’è‚µ‚Ä‚İ‚é
-	int num_good = 0; // ³‰ğ”
-	for (std::size_t i = 0, size = data.size(); i < size; ++i) {
-		std::vector<std::size_t> bin(iris_data::num_classes, 0);
-		for (std::size_t k = 0; k < num_decision_trees; ++k) {
-			++bin[(*trees[k])(data[i])];
-		}
+  // å…¨ã¦åˆ¤å®šã—ã¦ã¿ã‚‹
+  int num_good = 0; // æ­£è§£æ•°
+  for (std::size_t i = 0, size = data.size(); i < size; ++i) {
+    std::vector<std::size_t> bin(iris_data::num_classes, 0);
+    for (std::size_t k = 0; k < num_decision_trees; ++k) {
+      ++bin[(*trees[k])(data[i])];
+    }
 
-		std::cout << (i+1) << '\t';
-		for (std::size_t j = 0; j < iris_data::num_classes; ++j) {
-			for (std::size_t n = 0, count = bin[j]; n < count; ++n) {
-				std::cout << j;
-			}
-		}
-		std::cout << '\n';
+    std::cout << (i + 1) << '\t';
+    for (std::size_t j = 0; j < iris_data::num_classes; ++j) {
+      for (std::size_t n = 0, count = bin[j]; n < count; ++n) {
+        std::cout << j;
+      }
+    }
+    std::cout << '\n';
 
-		// “š‚¦
-		auto it = std::max_element(std::begin(bin), std::end(bin));
-		int const klass = static_cast<int>(std::distance(std::begin(bin), it));
-		num_good += (data[i].klass() == klass);
-	}
+    // ç­”ãˆ
+    auto it = std::max_element(std::begin(bin), std::end(bin));
+    int const klass = static_cast<int>(std::distance(std::begin(bin), it));
+    num_good += (data[i].klass() == klass);
+  }
 
-	// ³“š”
-	std::cout << num_good << '/' << data.size() << '\n';
+  // æ­£ç­”æ•°
+  std::cout << num_good << '/' << data.size() << '\n';
 
-	return 0;
+  return 0;
 }
