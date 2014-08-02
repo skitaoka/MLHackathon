6cc9e16ed8007875c8a09fece27543892a943e5a
Shinya Kitaoka <skitaoka@gmail.com>
2014-08-03 00:04:40 +0900

    fix bugs

--- 02_LatentDirichletAllocation/main.cpp
+++ 02_LatentDirichletAllocation/main.cpp
@@ -39,16 +39,16 @@ namespace reactor
 // K: トピック数
 // a: ハイパーパラメータ
 // b: ハイパーパラメータ
-void lda(
-  std::vector<std::size_t> const & w,
-  std::vector<std::size_t> const & n,
-  std::size_t const M,
-  std::size_t const V,
-  std::size_t const K,
-  double      const alpha,
-  double      const beta,
-  std::size_t const burn_in,
-  std::size_t const sample_size)
+std::pair<std::vector<double>, std::vector<double>>
+  lda(
+    std::vector<std::size_t> const & w,
+    std::vector<std::size_t> const & n,
+    std::size_t const M,
+    std::size_t const V,
+    std::size_t const K,
+    double      const alpha,
+    double      const beta,
+    std::size_t const sample_size)
 {
   std::mt19937_64 engine(std::random_device("")());
 
@@ -85,10 +85,8 @@ void lda(
     ++n_k[k];
   }
 
-  // 単語ごとのトピックの頻度
-  std::vector<std::size_t> q(w.size() * K);
-  for (std::size_t s = 0, ss = burn_in + sample_size; s < ss; ++s) {
-    // ギブスサンプリング
+  // ギブスサンプリング
+  for (std::size_t s = 0; s < sample_size; ++s) {
     for (std::size_t m = 0; m < M; ++m) {
       for (std::size_t i = n[m], count = n[m + 1]; i < count; ++i) {
         std::size_t const v = w[i];
@@ -114,15 +112,17 @@ void lda(
         ++n_mk[m * K + t];
         ++n_vk[v * K + t];
         ++n_k[t];
+
+        z[i] = t; // トピックを新しく設定
       }
     }
+  }
 
-    if (s >= burn_in) {
-      // カウント
-      for (std::size_t i = 0, size = z.size(); i < size; ++i) {
-        std::size_t const k = z[i];
-        ++q[i * K + k];
-      }
+  // トピックごとの単語分布をもとめる
+  std::vector<double> phi_kv(K * V);
+  for (std::size_t k = 0; k < K; ++k) {
+    for (std::size_t v = 0; v < V; ++v) {
+      phi_kv[k * V + v] = (n_vk[v * K + k] + beta) / (n_k[k] + V * beta);
     }
   }
 
@@ -135,28 +135,23 @@ void lda(
     }
   }
 
-  // トピックごとの単語分布をもとめる
-  std::vector<double> phi_kv(K * V);
-  for (std::size_t k = 0; k < K; ++k) {
-    for (std::size_t v = 0; v < V; ++v) {
-      phi_kv[k * V + v] = (n_vk[v * K + k] + beta) / (n_k[k] + K * beta);
-    }
-  }
+  return std::make_pair(std::move(theta_mk), std::move(phi_kv));
 }
 
 int main(int argc, char* argv[])
+try
 {
   // 人工的にデータをつくる
-  std::size_t const M =  20; // 文章数
-  std::size_t const V = 100; // 語彙数
-  std::size_t const K =   4; // トピック数
+  std::size_t const M =  100; // 文章数
+  std::size_t const V = 1000; // 語彙数
+  std::size_t const K =   10; // トピック数
 
-  double const alpha = 0.1;
+  double const alpha = 0.5;
   double const beta = 2.0;
 
   std::mt19937_64 engine(std::random_device("")());
 
-  std::vector<std::size_t> w(1000); // 単語
+  std::vector<std::size_t> w(M*V); // 単語
   std::vector<std::size_t> n(M + 1);
   for (std::size_t m = 1; m <= M; ++m) {
     n[m] = w.size() * m / M;
@@ -200,7 +195,31 @@ int main(int argc, char* argv[])
     }
   }
 
-  lda(w, n, M, V, K, alpha, beta, 100, 1000);
+  auto const theta_phi = lda(w, n, M, V, K, alpha, beta, 10000);
+
+  // 文章ごとのトピック分布
+  for (std::size_t m = 0; m < M; ++m) {
+    std::cout << m << "-th document:";
+    for (std::size_t k = 0; k < K; ++k) {
+      std::cout << ' ' << theta_phi.first[m * K + k];
+    }
+    std::cout << '\n';
+  }
+
+  // トピックごとの単語分布をもとめる
+  for (std::size_t k = 0; k < K; ++k) {
+    std::cout << k << "-th topic:";
+    for (std::size_t v = 0; v < V; ++v) {
+      std::cout << ' ' << theta_phi.second[k * V + v];
+    }
+    std::cout << '\n';
+  }
 
   return 0;
+}
+catch (std::exception const & e)
+{
+  std::cerr << e.what() << '\n';
+
+  return 1;
 }
